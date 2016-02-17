4950a11e73f38094fd195728d841ffcc4f2df648
zach georgii <zgeorgii@gmail.com>
2016-02-17 14:14:23 -0800

    getting heroku working

---
+++ scripts/repo.js
@@ -0,0 +1,21 @@
+(function(module) {
+  var repos = {};
+
+  repos.all = [];
+
+  // TODO: Refactor this ajax call into a get request to the proxy end point provided by server.js.
+  repos.requestRepos = function(callback) {
+    $.get('/github/users/brookr/repos' + '?per_page=100' + '&sort=updated')
+      .done(function(data, message, xhr) {
+        repos.all = data;
+    }).done(callback);
+  };
+
+  repos.with = function(attr) {
+    return repos.all.filter(function(repo) {
+      return repo[attr];
+    });
+  };
+
+  module.repos = repos;
+})(window);
